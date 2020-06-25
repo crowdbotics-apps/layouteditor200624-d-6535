4c51b9ae50235c1d32349380484f728ac9d32586
Crowdbotics <team@crowdbotics.com>
2020-06-25 13:26:24 +0000

    Update components

--- src/features/BlankScreen46671/screens/blank.js
+++ src/features/BlankScreen46671/screens/blank.js
@@ -18,7 +18,12 @@ export class _Blank extends React.Component {
     title: "Blank".toUpperCase()
   }
 
-  state = { Input_3: "", Input_29: "", Input_33: "" }
+  state = {
+    Input_3: "",
+    Datepicker_3: new Date("06/25/2020"),
+    Input_29: "",
+    Input_33: ""
+  }
 
   render = () => (
     <View
@@ -136,6 +141,7 @@ export class _Blank extends React.Component {
             placeholder="Sample Number Input Placeholder"
             editable={true}
             keyboardType="numeric"
+            textStyle={{ fontSize: 20, color: "#000000" }}
             style={{
               width: "100%",
               marginLeft: 0,
@@ -166,6 +172,38 @@ export class _Blank extends React.Component {
             }}
             value={this.state.Input_3}
             onChangeText={nextValue => this.setState({ Input_3: nextValue })}
+          />
+          <Datepicker
+            style={{
+              width: "100%",
+              marginLeft: 0,
+              marginRight: 0,
+              marginTop: 5,
+              marginBottom: 5,
+              paddingLeft: 5,
+              paddingRight: 5,
+              paddingTop: 5,
+              paddingBottom: 5,
+              overflow: "visible",
+              textAlign: "left",
+              verticalAlign: "baseline",
+              fontSize: 20,
+              color: "#000000",
+              backgroundColor: "#ffffff",
+              fontStyle: "normal",
+              fontWeight: "normal",
+              fontFamily: "System",
+              borderColor: "#000000",
+              borderStyle: "solid",
+              borderWidth: 0,
+              borderLeftWidth: 0,
+              borderRightWidth: 0,
+              borderTopWidth: 0,
+              borderBottomWidth: 0,
+              borderRadius: 0
+            }}
+            date={this.state.Datepicker_3}
+            onSelect={nextValue => this.setState({ Datepicker_3: nextValue })}
           />
         </View>
         <View
@@ -208,6 +246,7 @@ export class _Blank extends React.Component {
           <Input
             placeholder="Sample text input placeholder"
             editable={true}
+            textStyle={{ fontSize: 30, color: "#0000ff" }}
             style={{
               width: "100%",
               marginLeft: 0,
@@ -242,6 +281,7 @@ export class _Blank extends React.Component {
           <Input
             placeholder="Sample text input placeholder"
             editable={true}
+            textStyle={{ fontSize: 20, color: "#000000" }}
             style={{
               width: "100%",
               marginLeft: 0,
